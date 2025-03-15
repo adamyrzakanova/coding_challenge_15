33aaa00ce57f448f46f6e268f627629a9fc9f383
adamyrzakanova <aidaimyrzakanova@usf.edu>
2025-03-15 15:45:50 +0600

    Task 1 - Base Structure Setup.

---
+++ cc_15.js
@@ -0,0 +1,7 @@
+// Task 1 - Base Structure Setup
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Risk Dashboard Loaded");
+
+    const riskDashboard = document.getElementById("riskDashboard");
+    const riskForm = document.getElementById("riskForm");
+    const increaseRiskLevelsBtn = document.getElementById("increaseRiskLevels");
